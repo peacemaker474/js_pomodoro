bfe25b25fd0aa9795d0623d3e1080d91a140558b
{hc456123@gmail.com} <hc456123@gmail.com>
2021-10-09 21:17:23 +0900

    Add firebase SDK

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import GlobalStyle from './GlobalStyles';
 import Router from './Router';
+import firebase from "./firebase";
 
 function App() {
+  console.log(firebase);
   return (
     <>
       <GlobalStyle />
